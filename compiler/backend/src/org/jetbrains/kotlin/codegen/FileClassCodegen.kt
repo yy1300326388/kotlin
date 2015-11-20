3433634281f4242c42e3ca3a9a558efb1fcbd903
Dmitry Petrov <Dmitry.Petrov@jetbrains.com>
2015-11-20 14:34:57 +0300

    Refactor codegen for file classes.

--- compiler/backend/src/org/jetbrains/kotlin/codegen/FileClassCodegen.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/FileClassCodegen.kt
@@ -17,36 +17,41 @@
 package org.jetbrains.kotlin.codegen
 
 import com.intellij.util.ArrayUtil
+import org.jetbrains.kotlin.codegen.annotation.AnnotatedSimple
 import org.jetbrains.kotlin.codegen.context.FieldOwnerContext
 import org.jetbrains.kotlin.codegen.serialization.JvmSerializerExtension
 import org.jetbrains.kotlin.codegen.state.GenerationState
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
+import org.jetbrains.kotlin.descriptors.annotations.AnnotationDescriptor
+import org.jetbrains.kotlin.descriptors.annotations.AnnotationsImpl
 import org.jetbrains.kotlin.load.java.JvmAnnotationNames
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtProperty
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.serialization.DescriptorSerializer
+import org.jetbrains.kotlin.serialization.ProtoBuf
 import org.jetbrains.org.objectweb.asm.Opcodes
 import org.jetbrains.org.objectweb.asm.Type
 import java.util.*
 
-public class MultifileClassPartCodegen(
+public abstract class FileClassCodegen(
         v: ClassBuilder,
         file: KtFile,
-        private val filePartType: Type,
-        private val multifileClassType: Type,
-        partContext: FieldOwnerContext<*>,
-        state: GenerationState
-) : MemberCodegen<KtFile>(state, null, partContext, file, v) {
-    override fun generate() {
-        if (state.classBuilderMode == ClassBuilderMode.LIGHT_CLASSES) return
-        super.generate()
-    }
+        fileContext: FieldOwnerContext<*>,
+        state: GenerationState,
+        val filePartType: Type
+) : MemberCodegen<KtFile>(state, null, fileContext, file, v) {
+
+    protected abstract val classAttributes: Int
+
+    protected abstract fun generateAnnotationsForFileClass()
+
+    protected abstract fun generateKotlinAnnotationWithSerializedMembers(serializer: DescriptorSerializer, packageProto: ProtoBuf.Package)
 
     override fun generateDeclaration() {
         v.defineClass(element, Opcodes.V1_6,
-                      Opcodes.ACC_FINAL or Opcodes.ACC_SYNTHETIC,
+                      classAttributes,
                       filePartType.internalName,
                       null,
                       "java/lang/Object",
@@ -54,6 +59,8 @@ public class MultifileClassPartCodegen(
         v.visitSource(element.name, null)
 
         generatePropertyMetadataArrayFieldIfNeeded(filePartType)
+
+        generateAnnotationsForFileClass()
     }
 
     override fun generateBody() {
@@ -90,6 +97,67 @@ public class MultifileClassPartCodegen(
         )
         val packageProto = serializer.packagePartProto(members).build()
 
+        generateKotlinAnnotationWithSerializedMembers(serializer, packageProto)
+    }
+
+    override fun generateSyntheticParts() {
+        generateSyntheticAccessors()
+    }
+}
+
+public class PackagePartCodegen(
+        v: ClassBuilder,
+        file: KtFile,
+        filePartType: Type,
+        partContext: FieldOwnerContext<*>,
+        state: GenerationState
+): FileClassCodegen(v, file, partContext, state, filePartType) {
+    override val classAttributes: Int
+        get() = Opcodes.ACC_PUBLIC or Opcodes.ACC_FINAL
+
+    override fun generateAnnotationsForFileClass() {
+        val fileAnnotationDescriptors = ArrayList<AnnotationDescriptor>()
+        for (annotationEntry in element.annotationEntries) {
+            val annotationDescriptor = state.bindingContext.get(BindingContext.ANNOTATION, annotationEntry)
+            if (annotationDescriptor != null) {
+                fileAnnotationDescriptors.add(annotationDescriptor)
+            }
+        }
+        val annotatedFile = AnnotatedSimple(AnnotationsImpl(fileAnnotationDescriptors))
+        AnnotationCodegen.forClass(v.visitor, state.typeMapper).genAnnotations(annotatedFile, null)
+
+    }
+
+    override fun generateKotlinAnnotationWithSerializedMembers(serializer: DescriptorSerializer, packageProto: ProtoBuf.Package) {
+        val av = v.newAnnotation(AsmUtil.asmDescByFqNameWithoutInnerClasses(JvmAnnotationNames.KOTLIN_FILE_FACADE), true)
+        AsmUtil.writeAnnotationData(av, serializer, packageProto)
+        JvmCodegenUtil.writeModuleName(av, state)
+        av.visitEnd()
+    }
+
+}
+
+public class MultifileClassPartCodegen(
+        v: ClassBuilder,
+        file: KtFile,
+        filePartType: Type,
+        private val multifileClassType: Type,
+        partContext: FieldOwnerContext<*>,
+        state: GenerationState
+) : FileClassCodegen(v, file, partContext, state, filePartType) {
+    override val classAttributes: Int
+        get() = Opcodes.ACC_FINAL or Opcodes.ACC_SYNTHETIC
+
+    override fun generate() {
+        if (state.classBuilderMode == ClassBuilderMode.LIGHT_CLASSES) return
+        super.generate()
+    }
+
+    override fun generateAnnotationsForFileClass() {
+        // No annotations are generated for multifile class parts
+    }
+
+    override fun generateKotlinAnnotationWithSerializedMembers(serializer: DescriptorSerializer, packageProto: ProtoBuf.Package) {
         val av = v.newAnnotation(AsmUtil.asmDescByFqNameWithoutInnerClasses(JvmAnnotationNames.KOTLIN_MULTIFILE_CLASS_PART), true)
         AsmUtil.writeAnnotationData(av, serializer, packageProto)
         av.visit(JvmAnnotationNames.MULTIFILE_CLASS_NAME_FIELD_NAME, multifileClassType.internalName)
@@ -100,3 +168,4 @@ public class MultifileClassPartCodegen(
         generateSyntheticAccessors()
     }
 }
+

6a2b97da2e9c523c6ff4c0183db6e525665827bb
Pavel V. Talanov <talanov.pavel@gmail.com>
2016-01-19 19:05:50 +0300

    Resolve annotations on parameters of function type

---
+++ idea/testData/checker/regression/AnnotationOnNamedParameterOfFunctionType.kt
@@ -0,0 +1,5 @@
+fun <K, V> intercept(<warning>block</warning>: (<error descr="[UNSUPPORTED] Unsupported [annotation on parameter in function type]">@A</error> key: K, (K) -> V) -> V) {
+
+}
+
+annotation class A
